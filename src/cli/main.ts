e1288a5a91a3343cfd0dcd34c63bacd165cf4f57
Miedson Fernandes <miedson.fernandes@hotmail.com>
2024-01-29 11:59:35 -0300

    feat: CLI driver list command implementation

--- src/cli/main.ts
+++ src/cli/main.ts
@@ -2,24 +2,38 @@
 const program = require("commander");
 const pack = require("../../package.json");
 import CreateAccount from "../application/usecases/CreateAccount";
+import GetAllAccounts from "../application/usecases/GetAllAccounts";
 import PgPromise from "../infra/database/PgPromiseAdapter";
 import UserRepositorySql from "../infra/repositories/UserRepositorySql";
 
 program.version(pack.version);
 
-const data = [];
+const connection = new PgPromise();
+const userRository = new UserRepositorySql(connection);
 program
   .command("add [username,password,dateOfBirth]")
   .description("Adiciona uma nova conta")
   .action(async (data: string) => {
     const [username, password, dateofbirth] = data.split(",");
-    const connection = new PgPromise();
-    const userRository = new UserRepositorySql(connection);
     const createAccount = new CreateAccount(userRository);
     await createAccount.execute({
       username,
       password,
       dateofbirth: new Date(dateofbirth),
+    });
+  });
+
+  program.command("list")
+  .description("Lista os usuários")
+  .action(async () => {
+    const getAllAccounts = new GetAllAccounts(userRository);
+    const accounts = await getAllAccounts.execute();
+    console.log('Usuários cadastrados:');
+    accounts.forEach(account => {
+      console.log('');
+      console.log(`ID: ${account.id}`);
+      console.log(`Username: ${account.username}`);
+      console.log(`Data de nascimento: ${account.dateofbirth.toLocaleDateString('pt-br')}`);
     });
   });
 
